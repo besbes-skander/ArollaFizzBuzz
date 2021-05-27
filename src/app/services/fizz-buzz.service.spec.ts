c75557747cfe9d9737336803616566188889ee32
Skander <besbes.skander@gmail.com>
2021-05-27 15:15:40 +0200

    Test for 0

---
+++ src/app/services/fizz-buzz.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FizzBuzzService } from './fizz-buzz.service';
+
+describe('FizzBuzzService', () => {
+  let service: FizzBuzzService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FizzBuzzService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should return 0 for 0', () => {
+    const value = 0;
+
+    const result = service.outputFizzBuzz(value);
+
+    expect(result).toEqual(0);
+  });
+});
---
+++ src/app/services/fizz-buzz.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FizzBuzzService {
+
+  constructor() {
+  }
+
+  outputFizzBuzz(value: number) {
+    return value;
+  }
+}
